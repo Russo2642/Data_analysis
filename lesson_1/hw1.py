2f8f2d3beb0dae4d23b2da999b91139dd45968b1
daniyarsabaiev <daniyarsabaiev@gmail.com>
2021-05-27 20:45:46 +0600

    Мой первый комит

---
+++ lesson_1/hw1.py
@@ -0,0 +1,25 @@
+#Задача 1
+name = input('say your name: ')
+name = 'Hello, ' + name
+print(name)
+
+#Задача 2
+x = int(input('Enter number: '))
+y = int(input('Enter number: '))
+z = int(input('Enter number: '))
+print('sum of your numbers: ', x+y+z)
+
+#Задача 3
+a = int(input('укажи число'))
+b = int(input('укажи число'))
+print(a * b / 2)
+
+#Задача 4
+v = int(input('введи число'))
+print ('Следующее число после', v,'это',v+1)
+print ('Предыдущее число до',v,'это',v-1)
+
+
+
+
+
---
+++ lesson_2/lesson2.py
@@ -0,0 +1,80 @@
+
+
+
+# x = 30000
+# p = x * 0.4
+# v = x * 0.3
+# t = x * 0.3
+# print(f"""
+# Первый человек : {p}
+# Второй человек : {v}
+# Третий человек : {t}
+# """)
+
+# a = 3
+# b = 4
+# a, b = b, a
+# print (a,b)
+
+# a = int(input('введи число '))
+# b = int(input('введи число '))
+# c = int(input('введи число '))
+# if a + b > c or a + c > b or c + b > a:
+#     print('Существует')
+# else:
+#     print ('не сущ')
+
+
+# n = int(input('Enter num stud: '))
+# k = int(input('Enter num apples: '))
+# kor = k % n
+# yab = (k - kor)/n
+# print(yab, kor)
+
+# n = int(input('Enter num time: ')) 
+# ost = n % 60
+# ost1 = (n - ost) // 60
+# print(ost1,':', ost)
+
+# n = int(input('Enter num: '))
+# b = int(input('Enter num: '))
+# if n > b:
+#     print(n)
+# elif b > n:
+#     print(b)
+
+
+# x1 = int(input('CHESSEnter num: '))
+# y1 = int(input('CHESSEnter num: '))
+# x2 = int(input('CHEESEnter num: '))
+# y2 = int(input('CHESSEnter num: '))
+# if (x1 + y1 + x2 + y2) % 2 == 0:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# y = 2020
+# if y % 4 == 0 and y % 100 != 0 or y % 400 == 0:
+#     print('True')
+# else:
+#     print('False')
+
+# x = int(input('enter: '))
+# y1 = int(input('enter: '))
+# for x in range(x, y1 + 1):
+#     print(x)
+
+# x = int(input('enter: '))
+# y1 = int(input('enter: '))
+# if x < y1:
+#     for x in range(x, y1 + 1):
+#         print(x)
+# else:
+#     for x in range(x, y1 - 1, -1):
+#         print(x)
+
+
+for x in range(20, 0, -3):
+    if x % 2==0 and !=0
+print(x)
