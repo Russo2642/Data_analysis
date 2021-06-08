ef3d1eab06f54ed6c731bd343143e6f84dfc6d86
Ruslan Zulufov <stucci@mail.ru>
2021-06-08 22:05:11 +0600

    Пятнашки + задачи

--- lesson_5/game.py
+++ lesson_5/game.py
@@ -47,7 +47,10 @@ def is_game_finished(field):
     :param field: current field state.
     :return: True if the game is finished, False otherwise.
     """
+    param_field = list(range(1, 16))
+    param_field.append(EMPTY_MARK)
 
+    return field == param_field
 
 def perform_move(field, key):
     """
@@ -57,6 +60,22 @@ def perform_move(field, key):
     :return: new field state (after the move).
     :raises: IndexError if the move can't me done.
     """
+    index = field.index('x')
+    move = MOVES[key]
+    if key == 'w' and index <= 3:
+        raise IndexError('you cant move up')
+    elif key == 's' and index >= 12:
+        raise IndexError('you cant move down')
+    elif key == 'a' and (index % 4) == 0:
+        raise IndexError('you cant move left')
+    elif key == 'd' and (index % 4) == 3:
+        raise IndexError('you cant move right')
+    
+
+    new_index = index + move
+    field[index], field[new_index] = field[new_index], field[index]
+
+    return field
     
 
 def handle_user_input():
@@ -68,7 +87,11 @@ def handle_user_input():
         'd' - right
     :return: <str> current move.
     """
-    
+    step = input('Ход: ')
+    while not step in MOVES.keys():
+        step = input('Введите WASD.\nХод: ')
+    return step
+
 
 def main():
     """
@@ -78,10 +101,16 @@ def main():
     """
     field = shuffle_field()
     print_field(field)
-
+    while not is_game_finished(field):
+        key = handle_user_input()
+        try:
+            perform_move(field, key)
+        except IndexError as e:
+            print(e)
+        print_field(field)
 
 if __name__ == '__main__':
     # See what this means:
     # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
-
     main()
+
--- lesson_5/main.py
+++ lesson_5/main.py
@@ -1,10 +1,41 @@
-def function_with_default_value(name, age=0, needs_stars=True):
-    message = '{} is {} years old!'.format(name, age)
-    print(message)
+"""
+1.Написать функцию, которая принимает на вход два аргумента. Если аргументы больше нуля, возвращаем их сумму. Если оба меньше - разность. Если знаки разные - возвращаем 0
+"""
 
-    if needs_stars:
-        print('*****')
+def func(a, b):
+    if a > 0 and b > 0:
+        return a + b
+    elif a < 0 and b < 0:
+        return a - b
+    else:
+        return 0
 
-function_with_default_value('Nikita')
-function_with_default_value('Alex', age=19)
-function_with_default_value('Bob', age=40, needs_stars=False)
+
+# print(func(int(input('Введите число a: ')), int(input('Введите число b: '))))
+
+
+"""
+3.Написать функцию, которая принимает два аргумента. Первый - список чисел, второй - булевый флаг. 
+Если флаг действителен - возвращаем новый список с нечетными числами из входного списка, если флаг отрицателен -
+ возвращаем новый список с четными числами из входного списка
+"""
+
+
+def func(list_, Flag=True):
+    new_list_che = []
+    if Flag:
+        for x in list_:
+            if x % 2 != 0:
+                new_list_che.append(x)
+    return new_list_che
+
+
+def func1(lst, Flag=False):
+    new_list_neche = [x for x in lst if x % 2 == 0]
+    return new_list_neche
+
+
+list_ = [10, 2, 50, 5, -1, 0, 123]
+print(func(list_))
+
+print(func1(list_))
